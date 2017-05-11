114040d7534aaf175f8c4dae4be1c08ca9fa56c0
Sanchit B <sanchitb@mit.edu>
2017-05-10 22:53:13 -0400

    Working split-list-ingredients

--- ingredients.scm
+++ ingredients.scm
@@ -1,19 +1,30 @@
 (define (children node)
-  (filter list? (cdr node)))
+      (filter list? (cdr node)))
 
 (define (leaf? node)
-  (eq? (length (children node)) 0))
+  (and (list? node)  (eq? (length (children node)) 0)))
 
 (define (ingredient? node)
-  (and (leaf? node) (eq? (length node) 3)))
+  (and (list? node) (leaf? node) (eq? (length node) 3)))
 
 (define (split-ingredients-list nodelist)
-  (let ((curnode (car nodelist)))
-    (if (ingredient? curnode)
-	(append (list curnode) (split-ingredients-list (cdr nodelist)))
-	(begin
-	  (append (split-ingredients-list (children node))
-		  (split-ingredients-list (cdr nodelist)))))))
+  (if (and (list? nodelist) (not (null? nodelist)))
+      (let ((curnode (car nodelist)))
+	(if (ingredient? curnode)
+	    (cons curnode (split-ingredients-list (cdr
+							    nodelist)))
+	    (if (list? curnode)
+		(cons (split-ingredients-list (children curnode))
+			(split-ingredients-list (cdr nodelist)))
+		(split-ingredients-list (cdr nodelist)))))
+      '()))
 
-;Think this is more or less right but need to test and think about it
-;more, pushing atm so y'all have it
+;Tests work, although need to document them with better cases
+
+#|
+ (split-ingredients-list '(bake (a 3 "sdf") (b 4 "sdf")))
+;Value 13: ((a 3 "sdf") (b 4 "sdf"))
+
+ (split-ingredients-list '(bake (a 3 "sdf") (a 4 "sdf")))
+;Value 14: ((a 3 "sdf") (a 4 "sdf"))
+|#
