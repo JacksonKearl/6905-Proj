7b48b09c3ab5d5fc44fad90555d95e6db7d4037d
Sanchit B <sanchitb@mit.edu>
2017-05-11 19:51:25 -0400

    Imposed ingredient order, consolidating same ingredients, and unit consolidation

--- ingredients/ingredients.scm
+++ ingredients/ingredients.scm
@@ -29,31 +29,62 @@
 ;Value 14: ((a 3 "sdf") (a 4 "sdf"))
 |#
 
+;abstract into a single load function in top level
 (cd "5pset")
 (load "load.scm")
 (cd "../")
 
+(define (ingredient<? x y)
+  (if (and (ingredient? x) (ingredient? y))
+      (if (expr<? (car x) (car y))
+	  #t
+	  (if (eqv? (car x) (car y))
+	      (expr<? (caddr x) (caddr y))
+	      #f))
+      #f))
+
+#|
+ (ingredient<? '(a 4 c) '(b 3 b))
+;Value: #t
+
+ (ingredient<? '(a 4 c) '(a 5 b))
+;Value: #f
+|#
+  
 (define ingredient-simplifier
   (rule-simplifier
    (list
 
-    (rule `((?? x) ((? a) (? b ,number?) (? c)) (?? y)  ((? a) (? d
+    (rule `((?? x) (? a) (? b) (?? y))
+	  (and (ingredient<? b a)
+	       `(,@x ,b ,a ,@y)))
+
+    (rule `((?? x) ((? a) (? b ,number?) (? c)) ((? a) (? d
 							    ,number?)
 						   (? c))
 	    (?? z))
-	  `(,@x (,a ,(+ b d) ,c) ,@y ,@z))
+	  `(,@x (,a ,(+ b d) ,c) ,@z))
+
+    (rule `((?? x) ((? a) 0 (? c)) (?? y))
+	  `(,@x ,@y))
+
+    (rule `((?? x) ((? a) (? b ,number?) tbsp) ((? a) (? c ,number?) tsp) (?? y))
+	  `(,@x (,a ,(+ b (/ c 3)) tbsp) ,@y))
 
     )))
 
 #|
 
  (ingredient-simplifier '((a 3 g) (a 4 g)))
-;Value 15: ((a 7 g))
+;Value 38: ((a 7 g))
 
- (ingredient-simplifier  '((a 3 "sdf") (b 4 "sdf") (a 4 "sdf")))
-;Value 16: ((a 7 "sdf") (b 4 "sdf"))
+ (ingredient-simplifier  '((a 3 sdf) (b 4 sdf) (a 4 sdf)))
+;Value 39: ((a 7 sdf) (b 4 sdf))
 
- (ingredient-simplifier '((a 3 g) (a 4 h)))
-;Value 17: ((a 3 g) (a 4 h))
+ (ingredient-simplifier '((a 3 h) (a 4 g)))
+;Value 40: ((a 4 g) (a 3 h))
+
+ (ingredient-simplifier '((honey 5 tsp) (honey 3 tbsp)))
+;Value 45: ((honey 14/3 tbsp))
 
 |#
