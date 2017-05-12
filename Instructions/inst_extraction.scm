6facfb3b0767a7e3b084a098df8a43fa195f8d65
Cesar <guerrce@mit.edu>
2017-05-12 05:05:14 -0400

    split-instructions done, still working on full extraction

--- Instructions/inst_extraction.scm
+++ Instructions/inst_extraction.scm
@@ -1,16 +1,46 @@
 ;Instruction Extraction
 
+
+
 (define (instruction? node)
   (not (ingredient? node)))
 
+#|
+(instruction? '(bake (a 2 "cups") (b 3 "tsp")))
+;Value: #t
+
+(instruction? '(b 3 "tsp"))
+;Value: #f
+|#
+
+
+;represents order of instructions in reverse
 (define instructions-stack '())
 
+
 (define (push-onto-instructions-stack node)
-  (set! instruction-stack (append instruction-stack (list node)))
-  instruction-stack)
+  (set! instructions-stack (append instructions-stack (list node)))
+  instructions-stack)
+#|
+instructions-stack
+;Value ()
+
+(push-onto-instructions-stack '(bake (a 2 "cups")))
+;Value : ((bake (a 2 "cups")))
+
+(push-onto-instructions-stack '(mix (b 1 "tsp") (c 3 "tbsp")))
+;Value : ((bake (a 2 "cups")) (mix (b 1 "tsp") (c 3 "tbsp")))
+|#
+
 
 (define (instruction-as-ingredient instruction)
-  (string-append "ingredients that were " instruction))
+  (list (string->symbol (string-append "ingredients that were " (symbol->string instruction)))))
+
+#|
+(instruction-as-ingredient 'bake)
+;Value: |ingredients that were bake|
+|#
+
 
 (define (split-instructions recipe)
   (if (ingredient? recipe)
@@ -20,21 +50,30 @@
 	(if (eq? (length node-children) 0)
 	    (push-onto-instructions-stack instr)
 	    (if (ingredient? (car node-children))
-		(loop (append instr (car node-children))
+		(loop (append instr (list (car node-children)))
 		      (cdr node-children))
 		(begin (loop (append instr 
 				     (instruction-as-ingredient (caar node-children)))
 			     (cdr node-children))
-		       (split-instruction (car node-children))))))))
+		       (split-instructions (car node-children))))))))
 
 
 (define (instructions-in-order instructions)
   (if (eq? (length instructions) 1)
-      (car instructions)
+      (list (car instructions))
       (append (instructions-in-order (cdr instructions))
 	      (list (car instructions)))))
 
+#|
+(instructions-in-order '((a) (b) (c)))
+;Value : ((c) (b) (a))
+|#
+
+
 (define (extract-instructions recipe) 
   (instructions-in-order 
-   (split-instruction (recipe))))
+   (split-instructions (recipe))))
+
+
+
 
